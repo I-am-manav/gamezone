dd7f2179d4c621e7e72996acc5bbbb5a7912f3c5
I-am-manav <75670045+I-am-manav@users.noreply.github.com>
2026-06-10 01:18:52 +0530

    image path change from local to render

--- user/src/pages/Home.jsx
+++ user/src/pages/Home.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react"
 import { Link } from "react-router-dom"
 import { getGames, getFeedbacks } from "../services/api"
 
-const BACKEND = "http://localhost:8000"
+const BACKEND = import.meta.env.VITE_API_URL
 
 export default function Home() {
   const [games, setGames] = useState([])
